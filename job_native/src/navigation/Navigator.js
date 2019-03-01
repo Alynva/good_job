2271960554b5e0376fae6a0b837cf466f563a29a
Alisson Nunes <alynva@gmail.com>
2019-03-01 00:34:02 -0300

    Criado mecanismo de login

--- job_native/src/navigation/Navigator.js
+++ job_native/src/navigation/Navigator.js
@@ -1,31 +1,17 @@
 import React from "react"
-import { createDrawerNavigator, createAppContainer, createSwitchNavigator} from "react-navigation"
+import { createDrawerNavigator, createAppContainer } from "react-navigation"
 import LandingPageScreen from "../screens/LandingPageScreen"
 import CompanyPageScreen from "../screens/CompanyPageScreen"
+import LoginPageScreen from "../screens/LoginScreen"
 import SideBar from "./SideBar.js"
-
-// export default class Navigator extends React.Component {
-//     render() {
-//         return (
-//             <View></View>
-//         )
-//     }
-// }
-
-const LandingPageRouter = createSwitchNavigator(
-    {
-        LandingPage: { screen: LandingPageScreen, path : "LandingPage" },
-        CompanyPage: { screen: CompanyPageScreen, path : "CompanyPage" }
-    }, {
-        initialRouteName: "LandingPage"
-    }
-)
 
 export default createAppContainer(createDrawerNavigator(
     {
-        CompanyPage: LandingPageRouter
+        LandingPage: { screen: LandingPageScreen, path: "LandingPage" },
+        CompanyPage: { screen: CompanyPageScreen, path: "CompanyPage" },
+        LoginPage: { screen: LoginPageScreen, path: "LoginPage" }
     }, {
-        contentComponent: props => <SideBar {...props} />
-        
+        contentComponent: props => <SideBar {...props} />,
+        initialRouteName: "LandingPage"
     }
 ))
